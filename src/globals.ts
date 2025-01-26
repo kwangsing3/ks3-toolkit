ef0dafcf690bb09065a5e9e8b212d7c999871890
kwangsing3 <kwangsing3@gmail.com>
2025-01-26 21:46:37 +0800

    feat(argv): 建立輸入專案名稱及選擇標籤的功能。

---
+++ src/globals.ts
@@ -0,0 +1,6 @@
+// globals variables
+const globals: Record<string, string> = {
+  projectname: '',
+  projecttype: '',
+};
+export default globals;
--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,46 @@
 #!/usr/bin/env node
+/* eslint-disable n/no-extraneous-import */
 
-import {mkdirSync} from 'fs';
+import globals from './globals';
+import {confirm, input, select, Separator} from '@inquirer/prompts';
+import launchSequence from './launch.sequence';
+const choices = [
+  {
+    name: 'JavaScript',
+    value: 'javascript',
+    description: 'Create a Node.JS project with JavaScript Tookit',
+  },
+  {
+    name: 'TypeScript',
+    value: 'typescript',
+    description: 'Create a Node.JS project with TypeScript Tookit',
+  },
+  new Separator(),
+  {
+    name: 'Developer',
+    value: 'null',
+    disabled: 'view https://github.com/kwangsing3/ks3-toolkit',
+  },
+];
 
 (async () => {
-  console.log('Hello');
-  await mkdirSync('debugFolder');
+  //1. 決定專案名稱、類型(JS/TS)
+  globals.projectname = await input({message: '輸入創建的專案名稱'});
+  globals.projecttype = await select({
+    message: '選擇使用的語言類型 ',
+    choices: choices,
+  });
+  const answer = await confirm({message: 'Continue?'});
+  if (!answer) return;
+  //2. 依照標籤生成項目
+  await launchSequence();
 })()
-  .catch(() => {})
-  .finally(() => {});
+  .catch(err => {
+    console.error(err);
+  })
+  .finally(() => {
+    console.log('done');
+  });
 
 /*
  加入 prograss bar?
